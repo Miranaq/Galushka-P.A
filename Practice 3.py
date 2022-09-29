d259080685654123b71ccb391472b888b2192317
Miranaq <113391061+Miranaq@users.noreply.github.com>
2022-09-29 20:29:00 +0300

    Initial commit

---
+++ Practice 3.py
@@ -0,0 +1,12 @@
+a1 = int(input())
+b1 = int(input())
+c1 = int(input())
+s1 = a + b + c
+print(s1)
+a2 = int(input())
+b2 = int(input())
+print(a2 * b2 / 2)
+n = int(input())
+hrs = n % (60 * 24) // 60
+mns = n % 60
+print(hrs, mns)
