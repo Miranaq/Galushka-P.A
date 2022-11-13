59bc8575f09ad35edf5cbf251ec6241a490b8b61
Miranaq <113391061+Miranaq@users.noreply.github.com>
2022-11-13 09:55:38 +0300

    Initial commit

--- practice 8/pr8_v5_n1.py
+++ practice 8/pr8_v5_n1.py
@@ -3,12 +3,12 @@ A=int(input())
 B=int(input())
 C=int(input())
 D=int(input())
-e = A // D - B // C
-g = B * D
+e=A//D-B//C
+g =B*D
 a,b=e,g
-while a != b:
-    if a > b:
-        a -= b
+while a!=b:
+    if a>b:
+        a=a-b
     else:
-        b -= a
+        b=b-a
 print((e//a)/(g//a))
