e60ef755b307be75f567f6a0e754e1d5fd5b7396
Miranaq <113391061+Miranaq@users.noreply.github.com>
2022-11-17 21:35:14 +0300

    Initial commit

---
+++ practice 10/pr10_n1.py
@@ -0,0 +1,25 @@
+# -- coding: utf-8 --
+file = open("Galushka.P.A_Y223_vvod.txt", "r")
+mas = []
+while True:
+    x = file.readline().strip()
+    if (x == ""):
+        break
+    mas.append(str(x))
+
+end = []
+
+for i in range(len(mas)):
+    end.append([])
+    for j in mas[i]:
+        end[i].append(int(j))
+
+for i in range(len(end)):
+    end[i].sort()
+
+print("\n", end)
+with open("Galushka.P.A_Y223_vivod.txt", "w") as file:
+    for i in range(len(end)):
+        for j in range(len(end[i])):
+            file.write(str(end[i][j]))
+        file.write("\n")
